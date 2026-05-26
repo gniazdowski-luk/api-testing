0702e5a4c575119755ef47708668fb1e3ba712a2
Lukasz Gniazdowski <gniazdowski.luk@gmail.com>
2026-05-26 06:14:13 +0200

    test

--- tests/4_performance/users/users-get.spec.ts
+++ tests/4_performance/users/users-get.spec.ts
@@ -27,18 +27,6 @@ test('checks that a paginated request responds within the defined SLA @performan
   expect(elapsed).toBeLessThan(performanceData.slaMs.paginatedEndpoint);
 });
 
-test('checks that a full-text search request responds within the defined SLA @performance-users', async ({
-  request,
-  authHeaders,
-}) => {
-  const { response, elapsed } = await measureRequest(() =>
-    request.get(`users?q=${filteringUsersData.singleFirstname.value}`, { headers: authHeaders })
-  );
-
-  expect.soft(response.status()).toBe(200);
-  expect(elapsed).toBeLessThan(performanceData.slaMs.searchEndpoint);
-});
-
 test('checks that a sorted request responds within the defined SLA @performance-users', async ({
   request,
   authHeaders,
